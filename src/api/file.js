734a9851293d02b3aafecec1a38d37aed5a74c43
Glitch (rust-governor) <none>
2017-10-27 21:34:46 +0000

    initial setup

--- src/api/file.js
+++ src/api/file.js
@@ -1,3 +1,10 @@
 import express from 'express';
 export const fileApi= express.Router();
+import multer from 'multer';
+
+var upload = multer({ dest: 'uploads/' })
+
+fileApi.post('/fileanalyse',upload.single('upfile'),(req,res,next)=>{
+  res.json({filename:req.file.originalname,size:req.file.size});
+})
 
